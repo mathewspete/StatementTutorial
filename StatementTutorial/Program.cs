94d0b770a555d86cfac800b60ca77c259e07b36d
Pete Mathews <petemathews14@gmail.com>
2021-02-11 16:32:39 -0500

    Add project files.

---
+++ StatementTutorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StatementTutorial {
+    class Program {
+        static void Main(string[] args) {
+           // Console.WriteLine("Hello World!");
+
+           // Console.WriteLine("Enter a integer");
+           // var aNbr = Console.ReadLine();
+           ////  if (decimal.TryParse(, )
+           // var nbr = Convert.ToInt32(aNbr);
+           // if (nbr%2 == 0 ) {
+           //     Console.WriteLine($"The number {nbr} is even.");
+           // } else {
+           //     Console.WriteLine($"The number {nbr} is odd.");
+           // }
+
+
+
+
+            var ints = new[] { 77, 32, 94, 46, 28 };
+            for(var idx=0; idx<ints.Length; idx++) {
+                Console.WriteLine($"|idx| is {idx} and |ints[idx]| is {ints[idx]}.");
+            }
+        }
+    }
+}
