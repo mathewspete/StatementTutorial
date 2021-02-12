9d3d428a7e21ba8057d2b06483522859b0427f44
Pete Mathews <petemathews14@gmail.com>
2021-02-12 13:47:53 -0500

    foreach statements

---
+++ StatementTutorial/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatementTutorial {
+    class Song {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public DateTime Released { get; set; }
+        public bool Platinum { get; set; }
+
+    }
+}
